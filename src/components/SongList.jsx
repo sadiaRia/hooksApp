c6eaa0bd7c56f6beb39b619e2aa09d36bd7eb4db
sadia <sadia@techhivebd.com>
2020-04-02 00:29:22 +0600

    add component

---
+++ src/components/SongList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongList = () => {
+  return ( 
+    <div className="song-list">
+      <ul>
+        <li>Bella Ciao</li>
+        <li>High in the halls</li>
+      </ul>
+    </div>
+   );
+}
+ 
+export default SongList;
