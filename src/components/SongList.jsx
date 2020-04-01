a2def7ec949745e14d760d8d72a4af9866d2e3e7
sadia <sadia@techhivebd.com>
2020-04-02 01:05:36 +0600

    use useState

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SongList = () => {
-  return ( 
+  const [songs, setSongs] = useState([   // songs is variable & setSong is a function
+    { title: 'Bella Ciao', id: 1 },  // by using useState we keep the value into songs which is an array
+    { title: 'High in the halls', id: 2 },
+    { title: 'As long as you love me', id: 3 }
+  ])
+  const addSong = () => {
+    setSongs([...songs,{ title: 'new Songs', id: 4 }]); //for updating state we need to call that function 
+  }
+  return (
     <div className="song-list">
       <ul>
-        <li>Bella Ciao</li>
-        <li>High in the halls</li>
+        {songs.map(song =>
+          <li key={song.id}>{song.title}</li>
+        )}
       </ul>
+      <button onClick={addSong}>Add new Song</button>
     </div>
-   );
+  );
 }
- 
+
 export default SongList;
